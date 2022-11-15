22b995578f9ac087e59e31228625be7cdd9fb8f5
Adam Johnson <johnson.adamdouglas@gmail.com>
2022-11-15 17:09:15 -0500

    completed second step

---
+++ SQLJoinsExercise.sql
@@ -0,0 +1,27 @@
+/* joins: select all the computers from the products table:
+using the products table and the categories table, return the product name and the category name */
+ SELECT products.Name AS "Product Name", categories.Name AS "Category Name" 
+ FROM products 
+ INNER JOIN categories ON products.CategoryID = 1 AND categories.CategoryID = 1;
+/* joins: find all product names, product prices, and products ratings that have a rating of 5 */
+ Select products.Name AS "Product Name", price, reviews.Rating
+ FROM products 
+ LEFT JOIN reviews ON products.ProductID = reviews.ProductID
+ Where reviews.Rating = 5;
+/* joins: find the employee with the most total quantity sold.  use the sum() function and group by */
+
+/* joins: find the name of the department, and the name of the category for Appliances and Games */
+
+/* joins: find the product name, total # sold, and total price sold,
+ for Eagles: Hotel California --You may need to use SUM() */
+
+/* joins: find Product name, reviewer name, rating, and comment on the Visio TV. (only return for the lowest rating!) */
+
+
+-- ------------------------------------------ Extra - May be difficult
+/* Your goal is to write a query that serves as an employee sales report.
+This query should return:
+-  the employeeID
+-  the employee's first and last name
+-  the name of each product
+-  and how many of that product they sold */
